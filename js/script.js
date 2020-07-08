24a7c180b16d5d3b478feda7e74b7d08f2e185da
debs <deborah.chrystall.djon@outlook.com>
2020-07-08 16:30:15 +0200

    started on the 'add calendar entry function'. \n Added the form, outsourced the css. \n renamed some variables like events --> parsed events

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ function requestData(data) {
         if (this.readyState == 4 && this.status == 200) {
             switch(data) {
                 case("events"):
-                    loadEventTable(this);
+                    loadEventTable(this); //
                     break;
+
                 case("categories"):
                     loadCategoryTable(this);
                     break;
@@ -21,50 +22,53 @@ function requestData(data) {
 }
 
 function loadEventTable(json) {
-    var event = JSON.parse(json.responseText);
-
+    var parsed_events = JSON.parse(json.responseText);
     var events;
     var date;
     var time;
     var page;
     var image;
-    for (i in event) {
-        date = event[i].start.split("T", 1);
 
-        if (event[i].allday == true) {
+    console.log(parsed_events)
+
+    for (i in parsed_events) {
+        date = parsed_events[i].start.split("T", 1);
+
+        if (parsed_events[i].allday == true) {
             time = "All Day";
         } else {
-            time = event[i].start.split("T", 2)[1] + " - " + event[i].end.split("T", 2)[1];
+            time = parsed_events[i].start.split("T", 2)[1] + " - " + parsed_events[i].end.split("T", 2)[1];
         }
 
-        page = event[i].webpage.split(".", 2)[1];
+        page = parsed_events[i].webpage.split(".", 2)[1];
 
-        if(event[i].imageurl == null) {
+        if(parsed_events[i].imageurl == null) {
             img = "No Image";
         } else {
-            img = "<img src=\"" + event[i].imageurl + "\">";
+            img = "<img src=\"" + parsed_events[i].imageurl + "\">";
         }
 
         events = events + "<tr><td>" +
-            event[i].title + "</td><td>" +
-            event[i].status + "</td><td>" +
-            event[i].location + "</td><td>" +
-            "<a href=\"mailto:"+event[i].organizer+"\">"+event[i].organizer+"</a>" + "</td><td>" +
+            parsed_events[i].title + "</td><td>" +
+            parsed_events[i].status + "</td><td>" +
+            parsed_events[i].location + "</td><td>" +
+            "<a href=\"mailto:"+parsed_events[i].organizer+"\">"+parsed_events[i].organizer+"</a>" + "</td><td>" +
             date+"<br>"+time + "</td><td>" +
-            "<a href=\""+event[i].webpage+"\">"+page+"</a>" + "</td><td>" +
+            "<a href=\""+parsed_events[i].webpage+"\">"+page+"</a>" + "</td><td>" +
             img + "</td><td>" +
-            "<button onclick=\"editEvent()\">Edit</button>"+"<br>"+"<button onclick=\"deleteEvent()\">Delete</button>" + "</td></tr>";
+            "<button onclick=\"editEvent("+parsed_events[i].id+")\">Edit</button>"+"<br>"+"<button onclick=\"deleteEvent()\">Delete</button>" + "</td></tr>";
     }
-    document.getElementById("event_table").innerHTML = addEventHeader() + events;
+    document.getElementById("event_table").innerHTML = addTableHeader() + events;
 }
 
-function addEventHeader() {
+function addTableHeader() {
     var table="<tr><th>Title</th><th>Status</th><th>Location</th><th>Organizer</th><th>Date and Time</th><th>Webpage</th><th>Image</th><th>Actions</th></tr>";
     return table;
 }
 
-function editEvent() {
+function editEvent(i) {
     document.getElementById("testbutton").innerHTML += "Button Edit wurde gedrückt! <br>";
+    console.log(i);
 }
 
 function deleteEvent() {
@@ -72,5 +76,12 @@ function deleteEvent() {
 }
 
 function loadCategoryTable(json) {
+
+}
+
+function addEvent() {
+    document.getElementById("testbutton").innerHTML += "Add entry wurde gedrückt! <br>";
+    var form = document.createElement("form");
+
 
 }
